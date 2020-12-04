f15b82cacbfc6448b2d9672c1c8546cb36715ede
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2020-12-04 08:51:47 -0600

    Figure update 9
    Deprecated some old figure code; added comments to the per-seed recruitment plot code.

--- S2_DeprecatedCode.R
+++ S2_DeprecatedCode.R
@@ -1002,3 +1002,391 @@ for(i in seq(-1.3, max(CData.s$d.stand), length.out = 100)){
 # Clean up
 remove(lambda.i, TM, i)
 
+
+
+
+
+# ----- Flowering as a function of volume and density (1 of 4) --------------------------------------------
+
+# Remove entries for which there is no recorded volume
+# Then standardise density and introduce unique transect identifier
+CData.s <- CData %>% 
+  drop_na(volume_t) %>% 
+  mutate(d.stand = (weighted.dens - mean(weighted.dens, na.rm = TRUE)) / sd(weighted.dens, na.rm = TRUE),
+         unique.transect = interaction(transect, site))
+
+# Calculate range of data
+v.range <- range(CData.s$volume_t)
+d.range <- range(CData.s$d.stand)
+
+# Calculate predictions for 4 "bins" of volume categories
+v.cut <- CData.s %>% 
+  mutate(v.bin = cut_number(volume_t, n = 4)) %>% 
+  group_by(v.bin) %>% 
+  summarise(v.mean = mean(volume_t))
+d.and.v <- as.tibble(expand.grid(v.cut$v.mean, seq(d.range[1], d.range[2], 0.5)))
+names(d.and.v) <- c("v.mean", "x.d") 
+F.pred <- d.and.v %>% 
+  mutate(F.mean = invlogit(Mod.F.top.cf[1] + 
+                             Mod.F.top.cf[2] * v.mean + 
+                             Mod.F.top.cf[3] * x.d +
+                             Mod.F.top.cf[4] * v.mean * x.d +
+                             Mod.F.top.cf[5] * (x.d^2)+
+                             Mod.F.top.cf[6] * v.mean * (x.d^2)))
+
+# Get binned means of the data with respect to volume and density
+F.mean.df <- CData.s %>% 
+  mutate(v.bin = cut_number(volume_t, n = 4),
+         d.bin = cut_number(d.stand, n = 6)) %>% 
+  group_by(d.bin, v.bin) %>% 
+  summarise(F.mean = mean(did.flower),
+            d.mean = mean(d.stand),
+            v.mean = mean(volume_t))
+
+# Generate visualisation; flowering probability as function of density, with 4 volume bins
+F.pred %>% 
+  ggplot() +
+  geom_line(aes(x = x.d, y = F.mean, colour = as.factor(v.mean)), size = 3) +
+  scale_colour_manual(values = c("darkorchid2", "dodgerblue1", "navy", "firebrick2",
+                                 "darkorchid2", "dodgerblue1", "navy", "firebrick2")) +
+  geom_point(data = F.mean.df, aes(x = d.mean, y = F.mean, 
+                                   colour = as.factor(v.bin)), size = 7) +
+  labs(x = NULL, y = "Flowering Probability") +
+  ylim(0, 1) +
+  xlim(-1.5, 3) +
+  theme(legend.position = "none",
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(colour = "black", fill = NA, size = 2),
+        plot.background = element_rect(fill = "white"),
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(size = 38, margin = margin(t = 4, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(size = 38, margin = margin(t = 0, r = 4, b = 0, l = 0)),
+        axis.title.x = element_text(size = 43, margin = margin(t = 16, r = 0, b = 0, l = 6)),
+        axis.title.y = element_text(size = 43, margin = margin(t = 80, r = 16, b = 0, l = 0)),
+        axis.ticks.x = element_line(size = 0.5),
+        axis.ticks.y = element_line(size = 0.5),
+        axis.ticks.length = unit(8, "points")) -> Mod.F.top.dPlot
+
+# Do same process as previous 3 steps, but switch density and volume
+# This produces a graph of flowering probability as a function of volume, with 4 density bins
+d.cut <- CData.s %>% 
+  mutate(d.bin = cut_number(d.stand, n = 4)) %>% 
+  group_by(d.bin) %>% 
+  summarise(d.mean = mean(d.stand))
+d.and.v.2 <- as.tibble(expand.grid(d.cut$d.mean, seq(v.range[1], v.range[2], 0.5)))
+names(d.and.v.2) <- c("d.mean", "x.v") 
+F.pred <- d.and.v.2 %>% 
+  mutate(F.mean = invlogit(Mod.F.top.cf[1] + 
+                             Mod.F.top.cf[2] * x.v + 
+                             Mod.F.top.cf[3] * d.mean +
+                             Mod.F.top.cf[4] * x.v * d.mean +
+                             Mod.F.top.cf[5] * (d.mean^2) +
+                             Mod.F.top.cf[6] * x.v * (d.mean^2)))
+F.mean.df <- CData.s %>% 
+  mutate(d.bin = cut_number(d.stand, n = 4),
+         v.bin = cut_number(volume_t, n = 6)) %>% 
+  group_by(v.bin, d.bin) %>% 
+  summarise(F.mean = mean(did.flower),
+            d.mean = mean(d.stand),
+            v.mean = mean(volume_t))
+F.pred %>% 
+  ggplot() +
+  geom_line(aes(x = x.v, y = F.mean, colour = as.factor(d.mean)), size = 3) +
+  scale_colour_manual(values = c("darkorchid2", "firebrick2", "dodgerblue1", "darkorchid2",
+                                 "navy", "firebrick2", "dodgerblue1", "navy")) +
+  scale_y_continuous(position = "right") +
+  geom_point(data = F.mean.df, aes(x = v.mean, y = F.mean, 
+                                   colour = as.factor(d.bin)), size = 7) +
+  labs(x = NULL, y = NULL) +
+  theme(legend.position = "none",
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(colour = "black", fill = NA, size = 2),
+        plot.background = element_rect(fill = "white"),
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(size = 38, margin = margin(t = 4, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(size = 38, margin = margin(t = 0, r = 4, b = 0, l = 0)),
+        axis.title.x = element_text(size = 43, margin = margin(t = 16, r = 0, b = 0, l = 6)),
+        axis.title.y = element_text(size = 43, margin = margin(t = 80, r = 16, b = 0, l = 0)),
+        axis.ticks.x = element_line(size = 0.5),
+        axis.ticks.y = element_line(size = 0.5),
+        axis.ticks.length = unit(8, "points")) -> Mod.F.top.vPlot
+
+
+
+
+
+# ----- Growth as a function of volume and density (2 of 4) -----------------------------------------------
+
+# Restore CData.s since we deleted stuff earlier
+# Remove entries for which there is no recorded volume and log growth is NA
+# Then standardise density and introduce unique transect identifier
+CData.s <- CData %>% 
+  drop_na(volume_t, logGR) %>% 
+  mutate(d.stand = (weighted.dens - mean(weighted.dens, na.rm = TRUE)) / sd(weighted.dens, na.rm = TRUE),
+         unique.transect = interaction(transect, site))
+
+# Calculate range of data
+v.range <- range(CData.s$volume_t)
+d.range <- range(CData.s$d.stand)
+
+# Calculate predictions for 4 "bins" of volume categories
+v.cut <- CData.s %>% 
+  mutate(v.bin = cut_number(volume_t, n = 4)) %>% 
+  group_by(v.bin) %>% 
+  summarise(v.mean = mean(volume_t))
+d.and.v <- as.tibble(expand.grid(v.cut$v.mean, seq(d.range[1], d.range[2], 0.5)))
+names(d.and.v) <- c("v.mean", "x.d") 
+G.pred <- d.and.v %>% 
+  mutate(G.mean = Mod.G.top.cf[1] + 
+           Mod.G.top.cf[2] * v.mean + 
+           Mod.G.top.cf[3] * x.d +
+           Mod.G.top.cf[4] * v.mean * x.d +
+           Mod.G.top.cf[5] * (x.d^2)+
+           Mod.G.top.cf[6] * v.mean * (x.d^2))
+
+# Get binned means of the data with respect to volume and density
+G.mean.df <- CData.s %>% 
+  mutate(v.bin = cut_number(volume_t, n = 4),
+         d.bin = cut_number(d.stand, n = 6)) %>% 
+  group_by(d.bin, v.bin) %>% 
+  summarise(G.mean = mean(logGR),
+            d.mean = mean(d.stand),
+            v.mean = mean(volume_t))
+
+# Generate visualisation; growth ratio as function of density, with 4 volume bins
+G.pred %>% 
+  ggplot() +
+  geom_line(aes(x = x.d, y = G.mean, colour = as.factor(v.mean)), size = 3) +
+  scale_colour_manual(values = c("darkorchid2", "dodgerblue1", "navy", "firebrick2",
+                                 "darkorchid2", "dodgerblue1", "navy", "firebrick2")) +
+  scale_y_continuous(breaks = seq(0, 1, length.out = 5), limits = c(-0.1, 1)) +
+  geom_point(data = G.mean.df, aes(x = d.mean, y = G.mean, 
+                                   colour = as.factor(v.bin)), size = 7) +
+  labs(x = NULL, y = "Log Annual Growth Ratio") +
+  theme(legend.position = "none",
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(colour = "black", fill = NA, size = 2),
+        plot.background = element_rect(fill = "white"),
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(size = 38, margin = margin(t = 4, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(size = 38, margin = margin(t = 0, r = 4, b = 0, l = 0)),
+        axis.title.x = element_text(size = 43, margin = margin(t = 16, r = 0, b = 0, l = 6)),
+        axis.title.y = element_text(size = 43, margin = margin(t = 80, r = 16, b = 0, l = 0)),
+        axis.ticks.x = element_line(size = 0.5),
+        axis.ticks.y = element_line(size = 0.5),
+        axis.ticks.length = unit(8, "points")) -> Mod.G.top.dPlot
+
+# Do same process as previous 3 steps, but switch density and volume
+# This produces a graph of annual growth ratio as a function of volume, with 4 density bins
+d.cut <- CData.s %>% 
+  mutate(d.bin = cut_number(d.stand, n = 4)) %>% 
+  group_by(d.bin) %>% 
+  summarise(d.mean = mean(d.stand))
+d.and.v.2 <- as.tibble(expand.grid(d.cut$d.mean, seq(v.range[1], v.range[2], 0.5)))
+names(d.and.v.2) <- c("d.mean", "x.v") 
+G.pred <- d.and.v.2 %>% 
+  mutate(G.mean = Mod.G.top.cf[1] + 
+           Mod.G.top.cf[2] * x.v + 
+           Mod.G.top.cf[3] * d.mean +
+           Mod.G.top.cf[4] * x.v * d.mean +
+           Mod.G.top.cf[5] * (d.mean^2) +
+           Mod.G.top.cf[6] * x.v * (d.mean^2))
+G.mean.df <- CData.s %>% 
+  mutate(d.bin = cut_number(d.stand, n = 4),
+         v.bin = cut_number(volume_t, n = 6)) %>% 
+  group_by(v.bin, d.bin) %>% 
+  summarise(G.mean = mean(logGR),
+            d.mean = mean(d.stand),
+            v.mean = mean(volume_t))
+G.pred %>% 
+  ggplot() +
+  geom_line(aes(x = x.v, y = G.mean, colour = as.factor(d.mean)), size = 3) +
+  scale_colour_manual(values = c("darkorchid2", "firebrick2", "dodgerblue1", "darkorchid2",
+                                 "navy", "firebrick2", "dodgerblue1", "navy")) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.01), position = "right",
+                     breaks = seq(0, 1, length.out = 5), limits = c(-0.05, 1)) +
+  geom_point(data = G.mean.df, aes(x = v.mean, y = G.mean, 
+                                   colour = as.factor(d.bin)), size = 7) +
+  labs(x = NULL, y = NULL) +
+  theme(legend.position = "none",
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(colour = "black", fill = NA, size = 2),
+        plot.background = element_rect(fill = "white"),
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(size = 38, margin = margin(t = 4, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(size = 38, margin = margin(t = 0, r = 4, b = 0, l = 0)),
+        axis.title.x = element_text(size = 43, margin = margin(t = 16, r = 0, b = 0, l = 6)),
+        axis.title.y = element_text(size = 43, margin = margin(t = 80, r = 16, b = 0, l = 0)),
+        axis.ticks.x = element_line(size = 0.5),
+        axis.ticks.y = element_line(size = 0.5),
+        axis.ticks.length = unit(8, "points")) -> Mod.G.top.vPlot
+
+
+
+
+
+# ----- Reproduction as a function of volume and density (3 of 4) -----------------------------------------
+
+# Restore CData.s since we deleted stuff earlier
+# Remove entries for which there is no recorded volume or that did not flower
+# Then standardise density and introduce unique transect identifier
+CData.s <- CData %>%
+  drop_na(volume_t) %>%
+  filter(did.flower == 1) %>% 
+  mutate(d.stand = (weighted.dens - mean(weighted.dens, na.rm = TRUE)) / sd(weighted.dens, na.rm = TRUE),
+         unique.transect = interaction(transect, site))
+
+# Calculate range of data
+v.range <- range(CData.s$volume_t)
+d.range <- range(CData.s$d.stand)
+
+# Calculate predictions for 4 "bins" of volume categories
+v.cut <- CData.s %>% 
+  mutate(v.bin = cut_number(volume_t, n = 4)) %>% 
+  group_by(v.bin) %>% 
+  summarise(v.mean = mean(volume_t))
+d.and.v <- as.tibble(expand.grid(v.cut$v.mean, seq(d.range[1], d.range[2], 0.5)))
+names(d.and.v) <- c("v.mean", "x.d") 
+R.pred <- d.and.v %>% 
+  mutate(R.mean = Mod.R.top.cf[1] + 
+           Mod.R.top.cf[2] * v.mean + 
+           Mod.R.top.cf[3] * x.d +
+           Mod.R.top.cf[4] * v.mean * x.d +
+           Mod.R.top.cf[5] * (x.d^2) +
+           Mod.R.top.cf[6] * v.mean * (x.d^2))
+
+# Get binned means of the data with respect to volume and density
+R.mean.df <- CData.s %>% 
+  mutate(v.bin = cut_number(volume_t, n = 4),
+         d.bin = cut_number(d.stand, n = 6)) %>% 
+  group_by(d.bin, v.bin) %>% 
+  summarise(R.mean = mean(logTR1),
+            d.mean = mean(d.stand),
+            v.mean = mean(volume_t))
+
+# Generate visualisation; reproductive count as function of density, with 4 volume bins
+R.pred %>% 
+  ggplot() +
+  geom_line(aes(x = x.d, y = R.mean, colour = as.factor(v.mean)), size = 3) +
+  scale_colour_manual(values = c("darkorchid2", "dodgerblue1", "navy", "firebrick2",
+                                 "firebrick2", "darkorchid2", "dodgerblue1", "navy")) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.01)) +
+  geom_point(data = R.mean.df, aes(x = d.mean, y = R.mean, 
+                                   colour = as.factor(v.bin)), size = 7) +
+  labs(x = "Standardised Weighted Density", y = "Log Reproductive Structure Count") +
+  xlim(-1.5, 3) +
+  theme(legend.position = "none",
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(colour = "black", fill = NA, size = 2),
+        plot.background = element_rect(fill = "white"),
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(size = 38, margin = margin(t = 4, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(size = 38, margin = margin(t = 0, r = 4, b = 0, l = 0)),
+        axis.title.x = element_text(size = 43, margin = margin(t = 16, r = 0, b = 0, l = 6)),
+        axis.title.y = element_text(size = 43, margin = margin(t = 80, r = 16, b = 0, l = 0)),
+        axis.ticks.x = element_line(size = 0.5),
+        axis.ticks.y = element_line(size = 0.5),
+        axis.ticks.length = unit(8, "points")) -> Mod.R.top.dPlot
+
+# Do same process as previous 3 steps, but switch density and volume
+# This produces a graph of reproductive count as a function of volume, with 4 density bins
+d.cut <- CData.s %>% 
+  mutate(d.bin = cut_number(d.stand, n = 4)) %>% 
+  group_by(d.bin) %>% 
+  summarise(d.mean = mean(d.stand))
+d.and.v.2 <- as.tibble(expand.grid(d.cut$d.mean, seq(v.range[1], v.range[2], 0.5)))
+names(d.and.v.2) <- c("d.mean", "x.v") 
+R.pred <- d.and.v.2 %>% 
+  mutate(R.mean = Mod.R.top.cf[1] + 
+           Mod.R.top.cf[2] * x.v + 
+           Mod.R.top.cf[3] * d.mean +
+           Mod.R.top.cf[4] * x.v * d.mean +
+           Mod.R.top.cf[5] * (d.mean^2) +
+           Mod.R.top.cf[6] * x.v * (d.mean^2))
+R.mean.df <- CData.s %>% 
+  mutate(d.bin = cut_number(d.stand, n = 4),
+         v.bin = cut_number(volume_t, n = 6)) %>% 
+  group_by(v.bin, d.bin) %>% 
+  summarise(R.mean = mean(logTR1),
+            d.mean = mean(d.stand),
+            v.mean = mean(volume_t))
+R.pred %>% 
+  ggplot() +
+  geom_line(aes(x = x.v, y = R.mean, colour = as.factor(d.mean)), size = 3) +
+  scale_colour_manual(values = c("darkorchid2", "firebrick2", "dodgerblue1", "darkorchid2",
+                                 "navy", "firebrick2", "dodgerblue1", "navy")) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.01), position = "right",
+                     breaks = seq(-3, 9, length.out = 5), limits = c(-3, 9)) +
+  scale_x_continuous(breaks = c(5, 8, 12, 15)) +
+  geom_point(data = R.mean.df, aes(x = v.mean, y = R.mean, 
+                                   colour = as.factor(d.bin)), size = 7) +
+  labs(x = "Log-Volume", y = NULL) +
+  theme(legend.position = "none",
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(colour = "black", fill = NA, size = 2),
+        plot.background = element_rect(fill = "white"),
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(size = 38, margin = margin(t = 4, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(size = 38, margin = margin(t = 0, r = 4, b = 0, l = 0)),
+        axis.title.x = element_text(size = 43, margin = margin(t = 16, r = 0, b = 0, l = 6)),
+        axis.title.y = element_text(size = 43, margin = margin(t = 80, r = 16, b = 0, l = 0)),
+        axis.ticks.x = element_line(size = 0.5),
+        axis.ticks.y = element_line(size = 0.5),
+        axis.ticks.length = unit(8, "points")) -> Mod.R.top.vPlot
+
+
+
+
+
+# ----- Combine figures from the 3 sections above (4 of 4) ------------------------------------------------
+
+# Prepare graphics device
+jpeg(filename = "Figure 3.jpeg", width = 2600, height = 2950, units = "px")
+
+# Create blank page
+grid.newpage()
+plot.new()
+
+# Set grid layout and activate it
+gly <- grid.layout(2950, 2600)
+pushViewport(viewport(layout = gly))
+
+# Place graphs in a grid
+print(Mod.F.top.dPlot, vp = viewport(layout.pos.row = 1:950, layout.pos.col = 1:1300))
+print(Mod.F.top.vPlot, vp = viewport(layout.pos.row = 1:950, layout.pos.col = 1300:2600))
+print(Mod.G.top.dPlot, vp = viewport(layout.pos.row = 1000:1950, layout.pos.col = 1:1300))
+print(Mod.G.top.vPlot, vp = viewport(layout.pos.row = 1000:1950, layout.pos.col = 1300:2600))
+print(Mod.R.top.dPlot, vp = viewport(layout.pos.row = 2000:2950, layout.pos.col = 1:1300))
+print(Mod.R.top.vPlot, vp = viewport(layout.pos.row = 2000:2950, layout.pos.col = 1300:2600))
+
+# Create legend
+grid.rect(vp = viewport(layout.pos.row = 190:260, layout.pos.col = 1400:1470), gp = gpar(fill = "navy"))
+grid.rect(vp = viewport(layout.pos.row = 290:360, layout.pos.col = 1400:1470), gp = gpar(fill = "dodgerblue1"))
+grid.rect(vp = viewport(layout.pos.row = 390:460, layout.pos.col = 1400:1470), gp = gpar(fill = "darkorchid2"))
+grid.rect(vp = viewport(layout.pos.row = 490:560, layout.pos.col = 1400:1470), gp = gpar(fill = "firebrick2"))
+grid.text(label = c("[75, 100]", "[50, 75)", "[25, 50)", "[0, 25)"),
+          x = rep(0.580, 4), y = c(0.925, 0.892, 0.858, 0.823), just = "left", gp = gpar(fontsize = 37))
+grid.text(label = c("Volume percentile (left graphs)", "Density percentile (right graphs)"),
+          x = rep(0.537, 2), y = c(0.970, 0.952), just = "left", gp = gpar(fontsize = 40))
+
+# Deactivate grid layout; finalise graphics save
+popViewport()
+dev.off()
+
+# Clean up mess from global environment
+remove(d.and.v, d.and.v.2, d.cut, F.mean.df, F.pred, G.mean.df, G.pred, R.mean.df, R.pred,
+       v.cut, d.range, v.range, Mod.F.top.dPlot, Mod.F.top.vPlot, Mod.G.top.dPlot, 
+       Mod.G.top.vPlot, Mod.R.top.dPlot, Mod.R.top.vPlot, gly)
+
