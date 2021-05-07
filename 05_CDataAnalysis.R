888dbd0cfd3a5ff03825397b85b26b6326ea4fe8
Tom Miller <tom.miller@rice.edu>
2021-05-07 17:07:39 -0500

    Update 05_CDataAnalysis.R
    finished vital rate figure and printed pdf

--- 05_CDataAnalysis.R
+++ 05_CDataAnalysis.R
@@ -15,7 +15,7 @@ if(boot.switch == FALSE){
 
 
 ## set the gamma argument of gam() -- gamma>1 generates smoother fits, less likely to be overfit
-gamma = 1.2
+gamma = 1.8
 
 
 ##### Growth model ----------------------------------------------------------------------------------------
@@ -143,7 +143,7 @@ LATR_fruits[[1]] <- gam(total.reproduction_t ~ s(log_volume_t) + s(unique.transe
                         data = LATR_fruits_dat, gamma = gamma, family = "nb")
 LATR_fruits[[2]] <- gam(total.reproduction_t ~ s(log_volume_t) + s(weighted.dens) + s(unique.transect, bs = "re"),
                         data = LATR_fruits_dat, gamma = gamma, family = "nb")
-LATR_fruits[[3]] <- gam(total.reproduction_t ~ s(log_volume_t) + s(weighted.dens) + weighted.dens:log_volume_t + s(unique.transect, bs = "re"),
+LATR_fruits[[3]] <- gam(total.reproduction_t ~ s(log_volume_t) + s(weighted.dens) + ti(log_volume_t,weighted.dens) + s(unique.transect, bs = "re"),
                         data = LATR_fruits_dat, gamma = gamma, family = "nb")
 # Collect model AICs into a single table
 fruits_aic <- AICtab(LATR_fruits, base = TRUE, sort = FALSE)
@@ -190,11 +190,11 @@ points(log(LATR_surv_dat$volume_t[LATR_surv_dat$transplant == TRUE]),
 LATR_surv <- list()
 
 # Three candidate models for the mean: size only, size + density, or size, density, and size:density
-LATR_surv[[1]] <- gam(survival_t1 ~ s(log_volume_t) + transplant + s(unique.transect, bs = "re"),
+LATR_surv[[1]] <- gam(survival_t1 ~ s(log_volume_t,by=as.factor(transplant)) + s(unique.transect, bs = "re"),
                       data = LATR_surv_dat, gamma = gamma, family = "binomial")
-LATR_surv[[2]] <- gam(survival_t1 ~ s(log_volume_t) + s(weighted.dens)  + transplant + s(unique.transect, bs = "re"),
+LATR_surv[[2]] <- gam(survival_t1 ~ s(log_volume_t,by=as.factor(transplant)) + s(weighted.dens,by=as.factor(transplant))  + s(unique.transect, bs = "re"),
                       data = LATR_surv_dat, gamma = gamma, family = "binomial")
-LATR_surv[[3]] <- gam(survival_t1 ~ s(log_volume_t) + s(weighted.dens) + transplant + weighted.dens:log_volume_t + s(unique.transect, bs = "re"),
+LATR_surv[[3]] <- gam(survival_t1 ~ s(log_volume_t,by=as.factor(transplant)) + s(weighted.dens,by=as.factor(transplant)) + ti(log_volume_t,weighted.dens,by=as.factor(transplant)) + s(unique.transect, bs = "re"),
                       data = LATR_surv_dat, gamma = gamma, family = "binomial")
 
 # Collect model AICs into a single table
@@ -299,14 +299,70 @@ surv_aic
 recruit_aic
 
 
-# create vital rate figures ------------------------------------------------
+# create vital rate figure ------------------------------------------------
 size_breaks <- 4
 density_breaks <- 5
-## I get nicer colors if I double the breaks and take every other color
 LATR_cols <- wes_palette("Zissou1", size_breaks, type = "continuous")
-#LATR_cols <- rainbow(size_breaks)
 
-# growth figure -----------------------------------------------------------
+pdf("Manuscript/Figures/vital_rates.pdf",useDingbats = F,height=9,width=6)
+par(mfrow=c(1,3),mar=c(5,5,1,1))
+par(mfrow=c(3,2))
+## survival
+LATR_surv_dat %>% 
+  filter(transplant==F) %>% 
+  mutate(size_bin = as.numeric(cut(log_volume_t,breaks = size_breaks)),
+         density_bin = as.numeric(cut(weighted.dens,breaks = density_breaks))) %>% 
+  group_by(size_bin,density_bin) %>% 
+  summarise(mean_size = mean(log_volume_t),
+            mean_dens = mean(weighted.dens),
+            mean_surv = mean(survival_t1),
+            bin_n = n()) -> LATR_surv_plot_transF
+LATR_surv_plot_transF$size_col <- LATR_cols[LATR_surv_plot_transF$size_bin]
+
+mean_size <- LATR_surv_plot_transF %>% group_by(size_bin) %>% summarise(mean_size=mean(mean_size))
+surv_predict <- expand.grid(
+  weighted.dens = seq(min(LATR_surv_dat$weighted.dens),max(LATR_surv_dat$weighted.dens),1),
+  size_bin = mean_size$size_bin,
+  transplant=F,
+  unique.transect = LATR_flow_dat$unique.transect[1]
+)
+## associate sizes with bin numbers for plotting
+surv_predict$log_volume_t<-mean_size$mean_size[surv_predict$size_bin]
+surv_predict$pred <- predict.gam(LATR_surv_best, newdata = surv_predict, type = "response", exclude = "s(unique.transect)")
+surv_predict$size_col <- LATR_cols[surv_predict$size_bin]
+
+plot(LATR_surv_plot_transF$mean_dens,LATR_surv_plot_transF$mean_surv,col=alpha(LATR_surv_plot_transF$size_col,0.5),pch=16,
+     ylim=c(0,1),xlab="Weighted density",ylab="Survival probability",cex.lab=1.4,
+     cex=LATR_surv_plot_transF$bin_n/max(LATR_surv_plot_transF$bin_n)*2+1,
+     xlim=c(min(LATR_surv_dat$weighted.dens),max(LATR_surv_dat$weighted.dens)))
+points(surv_predict$weighted.dens,surv_predict$pred,col=surv_predict$size_col,pch=16,cex=0.75)
+title("A",adj=0)
+
+LATR_surv_dat %>% 
+  filter(transplant==T) %>% 
+  mutate(density_bin = as.numeric(cut(weighted.dens,breaks = density_breaks))) %>% 
+  group_by(density_bin) %>% 
+  summarise(mean_dens = mean(weighted.dens),
+            mean_surv = mean(survival_t1),
+            bin_n = n()) -> LATR_surv_plot_transT
+surv_trans_predict <- expand.grid(
+  weighted.dens = seq(min(LATR_surv_dat$weighted.dens),max(LATR_surv_dat$weighted.dens),1),
+  log_volume_t = mean(LATR_surv_dat$log_volume_t[LATR_surv_dat$transplant==T]),
+  transplant=T,
+  unique.transect = LATR_flow_dat$unique.transect[1]
+)
+surv_trans_predict$pred <- predict.gam(LATR_surv_best, newdata = surv_trans_predict, type = "response", exclude = "s(unique.transect)")
+
+plot(LATR_surv_plot_transT$mean_dens,LATR_surv_plot_transT$mean_surv,col=alpha("black",0.5),pch=16,
+     ylim=c(0,1),xlab="Weighted density",ylab="Transplant survival probability",cex.lab=1.4,
+     cex=LATR_surv_plot_transT$bin_n/max(LATR_surv_plot_transT$bin_n)*2+1,
+     xlim=c(min(LATR_surv_dat$weighted.dens),max(LATR_surv_dat$weighted.dens)))
+lines(surv_trans_predict$weighted.dens,surv_trans_predict$pred,lwd=3)
+#points(LATR_surv_dat$weighted.dens[LATR_surv_dat$transplant==T],LATR_surv_dat$survival_t1[LATR_surv_dat$transplant==T],
+#       col=alpha("black",0.5),pch="|")
+title("B",adj=0)
+
+## growth
 ## starting with raw data visualization, binning sizes and densities
 ## this plot freaked me out because it clearly shows positive density
 ## dependence at the smallest size -- no matter how you slice up sizes
@@ -321,10 +377,10 @@ LATR_grow %>%
             bin_n = n()) -> LATR_grow_plot
 LATR_grow_plot$size_col <- LATR_cols[LATR_grow_plot$size_bin]
 
-plot(LATR_grow_plot$mean_dens,LATR_grow_plot$mean_sizet1,col=LATR_grow_plot$size_bin,pch=16,cex=2)
+#plot(LATR_grow_plot$mean_dens,LATR_grow_plot$mean_sizet1,col=LATR_grow_plot$size_bin,pch=16,cex=2)
 ## however, I realized that the bins actually differ in initial size, and
 ## this can explain the apparent positive DD, here plotting the change in size to control for size differences
-plot(LATR_grow_plot$mean_dens,(LATR_grow_plot$mean_sizet1-LATR_grow_plot$mean_size),col=LATR_grow_plot$size_bin,pch=16,cex=2)
+#plot(LATR_grow_plot$mean_dens,(LATR_grow_plot$mean_sizet1-LATR_grow_plot$mean_size),col=LATR_grow_plot$size_bin,pch=16,cex=2)
 
 ## let's avoid binning the growth data for this read
 ## create dummy data frame for gam prediction
@@ -343,18 +399,18 @@ LATR_grow$size_col <- LATR_cols[as.numeric(cut(LATR_grow$log_volume_t,breaks = s
 grow_predict$size_col <- LATR_cols[grow_predict$size_bin]
 
 ## growth mean and SD inset
-plot(LATR_grow$weighted.dens,LATR_grow$log_volume_t1,pch=16,cex=0.5,
-     col=alpha(LATR_grow$size_col,0.75),xlab="Weighted density",ylab=expression(paste(Size[t+1]," (log ",cm^3,")")))
-points(grow_predict$weighted.dens,grow_predict$pred_mu,col=grow_predict$size_col,pch=16,cex=0.25)
-rect(100, -2.5, 220, 6, col="white")
+plot(LATR_grow$weighted.dens,LATR_grow$log_volume_t1,pch=16,cex=0.5,cex.lab=1.4,
+     col=alpha(LATR_grow$size_col,0.5),xlab="Weighted density",ylab=expression(paste(Size[t+1]," (log ",cm^3,")")))
+points(grow_predict$weighted.dens,grow_predict$pred_mu,col=grow_predict$size_col,pch=16,cex=0.75)
+rect(100, -2.5, 220, 6, col="white",lwd=0.5)
 plotInset(100, -1, 220, 6,
           expr=plot(grow_predict$weighted.dens,1/(grow_predict$pred_sigma^2),
                     col=grow_predict$size_col,pch=16,cex=0.25,xlab="",ylab=expression(paste("SD(",Size[t+1],")")),
                     cex.axis=0.5,mgp=c(3/2, 1/2, 0)),
           mar=c(0, 3, 0, 0))
+title("C",adj=0)
 
-
-# flowering figure --------------------------------------------------------
+## flowering
 LATR_flow_dat %>% 
   mutate(size_bin = as.numeric(cut(log_volume_t,breaks = size_breaks)),
          density_bin = as.numeric(cut(weighted.dens,breaks = density_breaks))) %>% 
@@ -367,20 +423,54 @@ LATR_flow_plot$size_col <- LATR_cols[LATR_flow_plot$size_bin]
 
 mean_size <- LATR_flow_plot %>% group_by(size_bin) %>% summarise(mean_size=mean(mean_size))
 flow_predict <- expand.grid(
-  weighted.dens = seq(min(LATR_flow_dat$weighted.dens),max(LATR_flow_dat$weighted.dens),50),
-  log_volume_t = mean_size$mean_size,
+  weighted.dens = seq(min(LATR_flow_dat$weighted.dens),max(LATR_flow_dat$weighted.dens),1),
+  size_bin = mean_size$size_bin,
   unique.transect = LATR_flow_dat$unique.transect[1]
 )
 ## associate sizes with bin numbers for plotting
+flow_predict$log_volume_t<-mean_size$mean_size[flow_predict$size_bin]
 flow_predict$pred <- predict.gam(LATR_flower_best, newdata = flow_predict, type = "response", exclude = "s(unique.transect)")
 flow_predict$size_col <- LATR_cols[flow_predict$size_bin]
 
-plot(LATR_flow_plot$mean_dens,LATR_flow_plot$mean_flow,col=LATR_flow_plot$size_col,pch=16,
+plot(LATR_flow_plot$mean_dens,LATR_flow_plot$mean_flow,col=alpha(LATR_flow_plot$size_col,0.5),pch=16,
      ylim=c(0,1),xlab="Weighted density",ylab="Flowering probability",
-     cex=LATR_flow_plot$bin_n/max(LATR_flow_plot$bin_n)*2+1,
+     cex=LATR_flow_plot$bin_n/max(LATR_flow_plot$bin_n)*2+1,cex.lab=1.4,
      xlim=c(min(LATR_flow_dat$weighted.dens),max(LATR_flow_dat$weighted.dens)))
-points(flow_predict$weighted.dens,flow_predict$pred,col=flow_predict$size_col,pch=16,cex=0.25)
+points(flow_predict$weighted.dens,flow_predict$pred,col=flow_predict$size_col,pch=16,cex=0.75)
+title("D",adj=0)
 
-plot(flow_predict$weighted.dens,flow_predict$pred,col=flow_predict$size_col,pch=16,cex=0.25,type="p")
+## fruits
+mean_size <- LATR_fruits_dat %>% 
+  mutate(size_bin=as.numeric(cut(log_volume_t,breaks = size_breaks))) %>% 
+  group_by(size_bin) %>% 
+  summarise(mean_size=mean(log_volume_t))
+fruits_predict <- expand.grid(
+  weighted.dens = seq(min(LATR_fruits_dat$weighted.dens),max(LATR_fruits_dat$weighted.dens),1),
+  size_bin = mean_size$size_bin,
+  unique.transect = LATR_fruits_dat$unique.transect[1]
+)
+## associate sizes with bin numbers for plotting
+LATR_fruits_dat$size_col <- LATR_cols[as.numeric(cut(LATR_fruits_dat$log_volume_t,breaks = size_breaks))]
+## note that these size breaks are *different* breaks for fruits than for the other vital rate (bc this is the flowering subset)
+fruits_predict$log_volume_t<-mean_size$mean_size[fruits_predict$size_bin]
+fruits_predict$pred <- predict.gam(LATR_fruits_best, newdata = fruits_predict, type = "response", exclude = "s(unique.transect)")
+fruits_predict$size_col <- LATR_cols[fruits_predict$size_bin]
 
+plot(LATR_fruits_dat$weighted.dens,LATR_fruits_dat$total.reproduction_t,pch=16,cex=0.5,ylim=c(0,4000),cex.lab=1.4,
+     col=alpha(LATR_fruits_dat$size_col,0.5),xlab="Weighted density",ylab="Flowers and fruits")
+points(fruits_predict$weighted.dens,fruits_predict$pred,col=fruits_predict$size_col,pch=16,cex=0.75)
+title("E",adj=0)
 
+## recruitment
+recruit_predict <- expand.grid(
+  weighted.dens = seq(min(LATR_recruitment$weighted.dens),max(LATR_recruitment$weighted.dens),1),
+  unique.transect = LATR_fruits_dat$unique.transect[1]
+)
+recruit_predict$pred <- predict.gam(LATR_recruit_best, newdata = recruit_predict, type = "response", exclude = "s(unique.transect)")
+
+plot(LATR_recruitment$weighted.dens,LATR_recruitment$recruits/LATR_recruitment$total_seeds,cex.lab=1.4,
+     col=alpha("black",0.5),pch=1,xlab="Weighted density",ylab="Per-seed recruitment probability",ylim=c(0,0.001))
+lines(recruit_predict$weighted.dens,recruit_predict$pred,lwd=3)
+title("F",adj=0)
+
+dev.off()
