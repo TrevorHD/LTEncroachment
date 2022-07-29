3a8350a3375e0d1d8e9d365329b7df06845edff8
Tom Miller <tom.miller@rice.edu>
2022-07-29 17:48:35 -0500

    it's flowering

---
+++ debug.R
@@ -0,0 +1,15 @@
+TM.growth(1,1,0:220,elas=1,sens=2)
+
+TM.survival(-2,0:220,elas=1,sens=2)
+
+plot(0:220,
+TM.flower(9.6,0:220,elas=1,sens=2)
+)
+
+plot(0:220,
+     TM.seeds(14,0:220,elas=1,sens=2)
+)
+
+plot(0:220,
+TM.recruitment(d=0:220,elas=1,sens=2)
+)
