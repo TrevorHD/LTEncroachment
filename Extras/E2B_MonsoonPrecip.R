0fe7164bf359267d3a595633ec4a9c041894b403
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2023-03-14 20:29:13 -0700

    File restructure 13
    Swapped file names so that the A file is the B file and vice-versa; originally had the order wrong, as one file defines the monsoon data and the other reads it.

--- Extras/E2B_MonsoonPrecip.R
+++ Extras/E2B_MonsoonPrecip.R
@@ -1,51 +1,40 @@
-##### Get monsoon precipitation data ----------------------------------------------------------------------------------------------------------------
+##### Load and plot monsoon precipitation -----------------------------------------------------------------------------------------------------------
 
-# KM Hall 2022-06-11
-# SEV monsoon precipitation (July-Sept.)
+# From Kris Hall: here is annual July-September monsoon precipitation by calendar year (not by water year)
+# for each of the met stations, with units in millimeters. 
 
-# Load packages
-library(tidyverse)
-library(lubridate)
+# Load data
+monsoon <- read.csv("Data/Weather/SEV_monsoon_precipitation.csv")
+str(monsoon)
 
-# Set folder where the output file will be written to
-path_for_data_export <- "~/Documents/SEV/Requests/Miller_Tom_monsoon_precip_20220611/"
+# Calculate seedlings per area by getting transect lengths
+transect_lengths <- read.csv("https://github.com/TrevorHD/LTEncroachment/raw/master/Data/LT_TransectLengths.csv") %>% 
+  mutate(area = Length_m*2)
+CData %>% 
+  group_by(site, transect,year_t1) %>% 
+  summarise(seedlings = sum(new.plant_t1 == 1 & seedling_t1 == 1, na.rm = T)) %>% 
+  rename(Site = site, Transect = transect) %>% 
+  left_join(., transect_lengths, by = c("Site", "Transect"))%>% 
+  group_by(year_t1) %>% 
+  summarise(tot_recruits = sum(seedlings),
+            tot_area = sum(area)) %>% 
+  mutate(recruits_per_area = tot_recruits/tot_area) -> recruits
 
-# Get meteorological data from EDI
-m8894 <- read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sev.1.15&entityid=87ff32c8e179e69743c6514cbbc8b31c",
-                  guess_max = 1000000)
-m9599 <- read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sev.1.15&entityid=dbe548a38a8dc4c1c829657cee190c4d",
-                  guess_max = 1000000)
-m0004 <- read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sev.1.15&entityid=371109f8068b35cf65edc8ba4237c8bd",
-                  guess_max = 1000000)
-m0509 <- read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sev.1.15&entityid=e326dbe48c0cdc5b91496a469a50e36d",
-                  guess_max = 1000000)
-m1014 <- read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sev.1.15&entityid=011fd6eb9726321cace6c72b50cb8056",
-                  guess_max = 1000000)
-m1519 <- read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sev.1.15&entityid=76922a0b041ac5ab05be6132ff7f90d7",
-                  guess_max = 1000000)
-m2021 <- read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sev.1.15&entityid=bd8e2163c866a22a72136db5b52dd3b9",
-                  guess_max = 1000000)
+# Prepare plotting to PDF
+pdf("Manuscript/Figures/monsoon_seedlings.pdf", useDingbats = F, height = 4, width = 8)
+par(mfrow = c(1, 2), mar = c(4, 4, 1, 1))
 
-# Combine data, view, and tidy
-m <- rbind(m8894, m9599, m0004, m0509, m1014, m1519, m2021)
-glimpse(m)
-m <- m %>% 
-  mutate(StationID = as.factor(StationID)) %>% 
-  select(StationID, Year, Month, Precipitation) %>% 
-  filter(Month %in% c(7, 8, 9)) %>% 
-  select(-Month)
+# Generate plots
+plot(monsoon$Year[monsoon$StationID == 49],
+     monsoon$Precipitation[monsoon$StationID == 49], type = "b",
+     ylab = "Monsoon (July-September) precip. (mm)", xlab = "Year")
+points(2013:2017, monsoon$Precipitation[monsoon$StationID == 49 & monsoon$Year %in% 2013:2017], pch = 16)
+title("A", adj = 0, font = 3)
+plot(monsoon$Precipitation[monsoon$StationID == 49 & monsoon$Year %in% 2013:2016],
+     recruits$recruits_per_area[recruits$year_t1 %in% 2014:2017],
+     xlab = "Monsoon precip. (mm)", ylab = "Recruit density per m2", cex = 2, pch = 16)
+title("B", adj = 0, font = 3)
 
-# Calculate monsoon precip (July-September) by calendar year
-ppt <- m %>% 
-  group_by(StationID, Year) %>% 
-  summarize(Precipitation = round(sum(Precipitation, na.rm = TRUE), 3)) %>% 
-  filter(StationID != "46" & StationID != "47")
-
-# Plot precipitation data
-ggplot(ppt, aes(x = Year, y = Precipitation, color = StationID)) +
-  geom_line() +
-  facet_wrap(~ StationID)
-
-# Write to file
-write_csv(ppt, paste0(path_for_data_export, "SEV_monsoon_precipitation.csv"))
+# Turn off graphics device
+dev.off()
 
